9a88d11e1edf2365e0a6043e80ec04e3e65ef424
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-30 18:52:56 -0400

    changes color and width

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,16 +3,16 @@ import React from 'react'
 export const ProgressBar = props => {
         const containerStyles = {
           height: 20,
-          width: '100%',
+          width: '80%',
           backgroundColor: "#e0e0de",
           borderRadius: 50,
-          margin: 0
+          margin: 'auto'
         }
       
         const fillerStyles = {
           height: '100%',
           width: `${props.progress}%`,
-          backgroundColor: 'aqua',
+          backgroundColor: '#45082d',
           borderRadius: 'inherit',
           transition: 'width 1s ease-in-out',
           textAlign: 'right'
