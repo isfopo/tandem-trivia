10357fd84fc37d7f1880596b4be1ecec7c5d94e9
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-29 21:34:13 -0400

    switches whether or not it is showing

--- src/components/ScoreDisplay.js
+++ src/components/ScoreDisplay.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ScoreDisplay = props => {
     return (
         <div className={props.showFinal ? "finalScoreDisplay" : "scoreDisplay" }>
-            { !props.showFinal &&
+            { props.showFinal &&
                 <h2>Your score:</h2>
             }
             <h1>{props.score}</h1>
