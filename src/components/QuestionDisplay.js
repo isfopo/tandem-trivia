c26e53a5f5b8b3a1506210b59ccec15dc1ffac5a
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-29 21:33:38 -0400

    asks question

---
+++ src/components/QuestionDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const QuestionDisplay = props => {
+    return (
+        <div className="question">
+            <p>{props.question}</p>
+        </div>
+    )
+}
