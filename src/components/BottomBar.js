04878174ee276f1636eca83bf70cff58037cf560
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-30 17:13:33 -0400

    adds button icons

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import "../styles/BottomBar.css";
+
+import restart from "../icons/autorenew-24px.svg";
+import next from "../icons/arrow_forward_ios-24px.svg";
 
 export const BottomBar = props => {
     return (
         <div className="bottomBar">
-            { props.canRestart &&
-                <button className="restartButton" onClick={() => props.restart()}>
-                    Restart
-                </button>
-            }
+            <button 
+                style={{visibility: props.canRestart ? "visible" : "hidden"}}
+                className="restartButton" 
+                onClick={() => props.restart()}>
+                    <img src={restart} alt="Restart" />
+            </button>
+        
 
-            { props.canNext && 
-                <button className="nextButton" onClick={() => props.next()}>
-                    Next
-                </button>
-            }
+            <button 
+                style={{visibility: props.canNext ? "visible" : "hidden"}}
+                className="nextButton" 
+                onClick={() => props.next()}>
+                    <img src={next} alt="Next" />
+            </button>
+            
         </div>
     )
 }
