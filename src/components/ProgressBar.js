a877f02e3948a4164edf2c79f42636afe69c8577
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-29 22:53:20 -0400

    inplements progressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-export const ProgressBar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+export const ProgressBar = props => {
+        const containerStyles = {
+          height: 20,
+          width: '100%',
+          backgroundColor: "#e0e0de",
+          borderRadius: 50,
+          margin: 0
+        }
+      
+        const fillerStyles = {
+          height: '100%',
+          width: `${props.progress}%`,
+          backgroundColor: 'aqua',
+          borderRadius: 'inherit',
+          transition: 'width 1s ease-in-out',
+          textAlign: 'right'
+        }
+      
+        return (
+          <div style={containerStyles}>
+            <div style={fillerStyles}></div>
+          </div>
+        )
 }
